acbf0e5df6259face87cded403882206c5c81d16
Muhammad Rizky Cavendio <72818583+Rizkycavendio@users.noreply.github.com>
2021-05-14 11:40:28 +0700

    Add files via upload

---
+++ PRAKTIKUM 6 - REKURSI NO. 1.cpp
@@ -0,0 +1,18 @@
+//Muhammad Rizky Cavendio - 20051397011
+
+#include <iostream>
+
+using namespace std;
+
+int main (){
+	int n,i;
+	cout<<"===================================================\n";
+	cout<<"Proses Rekursi menulis angka dari n sampai dengan 0\n";
+	cout<<"===================================================\n\n";
+	cout<<"Masukkan sebuah angka n, n= ";
+	cin>>i;
+	
+	for (n=i; n>=0; n--){
+	cout<<"outputnya="<<n<<endl;
+   }
+}
---
+++ PRAKTIKUM 6 - REKURSI NO. 2.cpp
@@ -0,0 +1,18 @@
+//Muhammad Rizky Cavendio - 20051397011
+
+#include <iostream>
+
+using namespace std;
+
+int main (){
+	int n,i;
+	cout<<"====================================================\n";
+	cout<<"Proses Rekursi menulis angka dari 0 sampai dengan n \n";
+	cout<<"====================================================\n\n";
+	cout<<"Masukkan sebuah angka n, n= ";
+	cin>>i;
+	
+	for (n=0; n<=i; n++){
+	cout<<"outputnya="<<n<<endl;
+	}
+}
