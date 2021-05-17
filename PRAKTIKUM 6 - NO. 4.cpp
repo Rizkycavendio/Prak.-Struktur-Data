d59ada289bde9b4355257e40ceb43af14dea4157
Muhammad Rizky Cavendio <72818583+Rizkycavendio@users.noreply.github.com>
2021-05-17 17:23:49 +0700

    Update PRAKTIKUM 6 - NO. 4.cpp

--- PRAKTIKUM 6 - NO. 4.cpp
+++ PRAKTIKUM 6 - NO. 4.cpp
@@ -7,20 +7,18 @@ using namespace std;
  
 int main()
 {
-   char string[25];   
+   char string[100];   
    int totstring;     
    cout << "=====================================================\n\n"; 
    cout << "       PROGRAM MEMBALIK KALIMAT DENGAN REKURSI       \n\n";
    cout << "=====================================================\n\n";
-   cout << "Masukkan Kalimat :"; 
-   cin  >> string;
-   cout << "Kalimat Terbalik : \n";
+   cout << "Masukkan Kalimat : ";cin>>string;
+   cout << "Kalimat Terbalik : ";
 
    totstring = strlen(string) - 1;
-   while (totstring >= 0)         
-   {
-    cout << string[totstring];
-     totstring = totstring - 1;  
+   while (totstring >= 0){
+   cout << string[totstring];
+   totstring = totstring - 1;  
    }
    getch();
 }
